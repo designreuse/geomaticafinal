e9f43356a89a9038b0569714387e22783ee6d4f5
root <henryalejo@gmail.com>
2015-05-26 12:13:00 -0500

    first succ

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
 (function (){
 
-	var app = angular.module('angularmaps',[])
+	var app = angular.module('angularmaps',['ngSanitize'])
+	
+	.config(function($sceDelegateProvider,$compileProvider) {
+		//$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|local|data|ftp|file):/);
 
-	.controller('mapsController', function($scope){
+		$sceDelegateProvider.resourceUrlWhitelist([
+		    // Allow same origin resource loads.
+		    'self',
+		    // Allow loading from our assets domain.  Notice the difference between * and **.
+		    'https://maps.googleapis.com/maps/api/**',
+			'https://*.google.com/**',
+			'https://*.gstatic.com/**'
+	  	]);
+		  
+		  })
+		  
+	.controller('mapsController', function($scope,$http,$q,$timeout){
 		var directionsDisplay;
 		var directionsService = new google.maps.DirectionsService();
 		var map;
 		var tsp ;
+		var combinedResults=new Array();
+		var rendererOptions = {
+			preserveViewport: true,         
+			//suppressMarkers:true,
+			//routeIndex:0
+			};
+
 		$scope.start="";
 		$scope.stop="";
 		$scope.centercity="";
@@ -25,7 +46,7 @@
 		$scope.sendMode="";
 		$scope.distance=0;
 		$scope.initialize = function () {
-		  directionsDisplay = new google.maps.DirectionsRenderer();
+		  directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
 			$scope.chicago = { 
 				name: "Chicago",
 				coords: new google.maps.LatLng(41.850033, -87.6500523)
@@ -139,16 +160,18 @@
 
 			$scope.sendMode=mode;
 		};
-		
+
 		$scope.calcRoute = function () {
+			var timeoutcount=-1;
+			var requestarray= new Array();
 			var iterations= Math.ceil($scope.waypoints.length/10);
 			var order=new Array();
 			//tsp
 		  
 		  	tsp.solveAtoZ(function (params) {
 			  createPolyline(tsp.getGDirections());			  
-			  console.log("En orden");
-			 console.log(tsp.getOrder());
+			  //console.log("En orden");
+			 //console.log(tsp.getOrder());
 			 order=tsp.getOrder();
 			 var tempwaypoints= new Array();
 			for (var index = 0; index < order.length; index++) {
@@ -156,52 +179,105 @@
 				 tempwaypoints.push($scope.waypoints[order[index]]);
 				
 			}		
-			 	 console.log("wayponits");
-				  console.log($scope.waypoints);
-				  console.log("tempway");
-				 console.log(tempwaypoints);
+			 
 			 for (var numrequest = 0; numrequest < iterations; numrequest++) {
 				 
-				 
+				var temprequest=numrequest;
+				
+				 //console.log("Para mirar el error mirar el request, el numrequest:"+ numrequest);
 				  var temp=new Array();
-				  var end=(numrequest+1)*10;
+				  var temprequest=numrequest;
+				  var end=(temprequest+1)*10;
+				  
 				  var start=end-10;
-				  temp= tempwaypoints.slice(start,end);
-				  var temporigin=tempwaypoints[0].location;
+				  //console.log("Para mirar el error mirar el request, el numrequest:"+ numrequest);
+				  temp.push(tempwaypoints.slice(start,end));
+				  //console.log("the temp");
+				  //console.log(temp);
+				  var temporigin=temp[0][0].location;
+				  //console.log("Longitud del temp");
+				  //console.log(temp[0].length);
+				  var tempdestination=temp[0][temp[0].length-1].location;
+				  var tempstops=temp[0].slice(1,temp[0].length-1);
 				  
-				  var tempdestination=tempwaypoints[tempwaypoints.length-1].location;
-				  var tempstops=tempwaypoints.slice(1,tempwaypoints.length-1);
-				  
-				  console.log("el origen: ");
-				  console.log(temporigin);
-				  
-				  console.log("el destion: ");
-				  console.log(tempdestination);
-				  console.log("el stops: ");
-				  console.log(tempstops);
-				  
-				  $scope.request = {
-			      origin:	temporigin,
-			      destination:tempdestination,
-			      
-			      travelMode: google.maps.TravelMode[$scope.sendMode],
-			      waypoints: tempstops,
-			      optimizeWaypoints: true
-	
+				  var request = {
+					      origin:	temporigin,
+					      destination:tempdestination,					      
+					      travelMode: google.maps.TravelMode[$scope.sendMode],
+					      waypoints: tempstops,
+					      optimizeWaypoints: true
+			
 			      
 			  		};
-				  directionsService.route($scope.request, function(response, status) {
-				    if (status == google.maps.DirectionsStatus.OK) {
-				      directionsDisplay.setDirections(response);
-				      console.log(response);
-				      console.log(response.routes[0].legs[0].distance.value);
-				      $scope.getDistance(response.routes[0].legs);
-				    }
-				  });
-				  
+					requestarray.push(request);
+				
+				console.log("Para mirar el error mirar el request, el numrequest:");
+				console.log(temprequest);
+				//temprequest=numrequest;
+				$timeout( function () {
+				timeoutcount=timeoutcount+1;
+				var deferred = $q.defer();	 
+				var	directionsServicetemp = new google.maps.DirectionsService();
+					  directionsServicetemp.route(requestarray[timeoutcount], function(response, status) {
+						  	console.log("Para mirar el request dentro del route:");	
+							console.log(request);
+						  
+						  //temprequest=numrequest;
+						  
+					    if (status == google.maps.DirectionsStatus.OK) {
+							//console.log("El response: ");
+						 //console.log(response);
+						deferred.resolve(response);
+						combinedResults.push(deferred.promise);
+							
+						 console.log("combinedResults:");
+						 console.log(combinedResults);
+						
+						 //console.log("El numrequest: ");
+						 //console.log(temprequest);
+						   combinedResults[combinedResults.length-1].then(function (data) {
+							   //console.log("El response: ");
+						 		//console.log(response);
+							   //combinedResults.push(data);
+							   //console.log("Promesa numero: " );
+							   //console.log(temprequest);
+							   //temprequest=numrequest;
+							   //console.log("El data: ");
+							   //console.log(data);
+							   createBluePolyline(response);
+							   //console.log(combinedResults);
+							   
+							   console.log(" longitud array promesas");
+							  console.log(combinedResults.length);
+							  
+							 /*
+							  if(combinedResults.length==(temprequest+1)){
+								  console.log("hola mundo del dibujo");
+								  for (var index = 0; index < combinedResults.length; index++) {
+									  console.log(data);									   
+									   createBluePolyline(data);
+									   };								  
+								   };
+								  */ 
+								   
+								   
+							 });
+				  				
+					    }//direction status
+					  }
+					  )
+					
+										
+				  }, ((temprequest+1)*1000)-999 );	
+				  //temprequest=numrequest;
 				  
 			  }
+			  
 			 
+			  
+			 //directionsDisplay.setDirections(combinedResults[1]);
+				     
+				      //$scope.getDistance(response.routes[0].legs);
 			 
 			 
 			 
@@ -211,7 +287,145 @@
 		  
 		
 		  $scope.clearMarkers ();
-		}
+		  //directionsDisplay.setDirections(combinedResults[1]);
+		  //createBluePolyline(combinedResults[1]);
+		};
+
+		
+		$scope.calcRoute3 = function () {
+			
+			var iterations= Math.ceil($scope.waypoints.length/10);
+			var order=new Array();
+			//tsp
+		  
+		  	tsp.solveAtoZ(function (params) {
+			  createPolyline(tsp.getGDirections());			  
+			  //console.log("En orden");
+			 //console.log(tsp.getOrder());
+			 order=tsp.getOrder();
+			 var tempwaypoints= new Array();
+			for (var index = 0; index < order.length; index++) {
+				
+				 tempwaypoints.push($scope.waypoints[order[index]]);
+				
+			}		
+			 	 //console.log("wayponits");
+				  //console.log($scope.waypoints);
+				  //console.log("tempway");
+				 //console.log(tempwaypoints);
+			 for (numrequest = 0; numrequest < iterations; numrequest++) {
+				 var temprequest=numrequest;
+				 //console.log("Para mirar el error mirar el request, el numrequest:"+ numrequest);
+				  var temp=new Array();
+				  var temprequest=numrequest;
+				  var end=(temprequest+1)*10;
+				  
+				  var start=end-10;
+				  //console.log("Para mirar el error mirar el request, el numrequest:"+ numrequest);
+				  temp.push(tempwaypoints.slice(start,end));
+				  //console.log("the temp");
+				  //console.log(temp);
+				  var temporigin=temp[0][0].location;
+				  //console.log("Longitud del temp");
+				  //console.log(temp[0].length);
+				  var tempdestination=temp[0][temp[0].length-1].location;
+				  var tempstops=temp[0].slice(1,temp[0].length-1);
+				  
+				  //console.log("el origen: ");
+				  //console.log(temporigin);
+				  
+				  //console.log("el destion: ");
+				  //console.log(tempdestination);
+				  //console.log("el stops: ");
+				  //console.log(tempstops);
+				  
+				  $scope.request = {
+					      origin:	temporigin,
+					      destination:tempdestination,
+					      
+					      travelMode: google.maps.TravelMode[$scope.sendMode],
+					      waypoints: tempstops,
+					      optimizeWaypoints: true
+			
+			      
+			  		};
+					/*  
+					$http.get("https://maps.googleapis.com/maps/api/directions/json?key=AIzaSyBKeftt9o3yj6eK7oro-mvAMqgetdf75Ck&",$scope.request).then( function(data){
+						combinedResults.push(data.data);
+					});	  */
+					
+				//console.log("Para mirar el error mirar el request, el numrequest:"+ numrequest);
+				console.log($scope.request);
+				console.log("Para mirar el error mirar el request, el numrequest:");
+				console.log(temprequest);
+				//temprequest=numrequest;
+				//$timeout( function () {
+				var deferred = $q.defer();	 
+					
+					  directionsService.route($scope.request, function(response, status) {
+						  //temprequest=numrequest;
+						  deferred.resolve(response);
+					    if (status == google.maps.DirectionsStatus.OK) {
+						combinedResults.push(deferred.promise);
+							
+						 console.log("combinedResults");
+						  console.log(combinedResults);
+						 
+						   combinedResults[numrequest].then(function (data) {
+							   //combinedResults.push(data);
+							   //console.log("Promesa numero: " );
+							   //console.log(temprequest);
+							   //temprequest=numrequest;
+							   //console.log("El data: ");
+							   //console.log(data);
+							   //createBluePolyline(data);
+							   //console.log(combinedResults);
+							   
+							   console.log(" longitud");
+							  console.log(combinedResults.length);
+							  if(combinedResults.length==(temprequest+1)){
+								  console.log("hola mundo");
+								  for (var index = 0; index < combinedResults.length; index++) {
+									  console.log(data);
+									   
+									   createBluePolyline(data);
+									  
+									  
+								  };
+								  
+								  
+							  };
+							   
+							   
+						   });
+				  				
+					    }//direction status
+					  }
+					  )
+					
+										
+				  //}, ((temprequest+1)*1000)-999 );	
+				  //temprequest=numrequest;
+				  
+			  }
+			  
+			 
+			  
+			 //directionsDisplay.setDirections(combinedResults[1]);
+				     
+				      //$scope.getDistance(response.routes[0].legs);
+			 
+			 
+			 
+		  	});
+			
+					  
+		  
+		
+		  $scope.clearMarkers ();
+		  //directionsDisplay.setDirections(combinedResults[1]);
+		  //createBluePolyline(combinedResults[1]);
+		};
 		
 		
 		$scope.calcRoute2 = function () {
@@ -240,8 +454,9 @@
 		  directionsService.route($scope.request, function(response, status) {
 		    if (status == google.maps.DirectionsStatus.OK) {
 		      directionsDisplay.setDirections(response);
-		      console.log(response);
-		      console.log(response.routes[0].legs[0].distance.value);
+			  
+		      //console.log(response);
+		      //console.log(response.routes[0].legs[0].distance.value);
 		      $scope.getDistance(response.routes[0].legs);
 		    }
 		  });
@@ -253,8 +468,8 @@
 		  tsp.solveAtoZ(function (params) {
 			  createPolyline(tsp.getGDirections());
 			  
-			   console.log("En orden");
-			  console.log(tsp.getOrder());
+			   //console.log("En orden");
+			  //console.log(tsp.getOrder());
 		  });
 		  //
 		  $scope.clearMarkers ();
@@ -307,7 +522,7 @@
 			      path: directionResult.routes[0].overview_path,
 			      strokeColor: '#FF0000',
 			      strokeOpacity: 0.5,
-			      strokeWeight: 4
+			      strokeWeight: 2
 			  });
 			
 			  line.setMap(map);
@@ -320,6 +535,29 @@
 			      });
 			  }
 			}
+			function createBluePolyline(directionResult) {
+			  var line = new google.maps.Polyline({
+			      path: directionResult.routes[0].overview_path,
+			      strokeColor: '#3300FF', 
+			      strokeOpacity: 0.5,
+			      strokeWeight: 6
+			  });
+			
+			  line.setMap(map);
+			/*
+			  for (var i = 0; i < line.getPath().length; i++) {
+			      var marker = new google.maps.Marker({
+			          icon: {
+						   //path: google.maps.SymbolPath.CIRCLE, scale: 3 
+						   
+						   },  
+			          position: line.getPath().getAt(i),
+			          map: map
+			      });
+			  }
+			  */
+			}
+
 
 		google.maps.event.addDomListener(window, 'load', $scope.initialize);
 
---
+++ some.js
@@ -0,0 +1,135 @@
+$(function() {
+    var stops = [{"Geometry":{"Latitude":51.507937,"Longitude":-0.076188}},{"Geometry":{"Latitude":51.51168,"Longitude":-0.114155}},{"Geometry":{"Latitude":51.5010063,"Longitude":-0.041407}}] ;
+
+    var map = new window.google.maps.Map(document.getElementById("map"));
+
+    // new up complex objects before passing them around
+    var directionsDisplay = new window.google.maps.DirectionsRenderer();
+    var directionsService = new window.google.maps.DirectionsService();
+
+    Tour_startUp(stops);
+
+    window.tour.loadMap(map, directionsDisplay);
+    window.tour.fitBounds(map);
+
+    if (stops.length > 1)
+        window.tour.calcRoute(directionsService, directionsDisplay);
+});
+
+function Tour_startUp(stops) {
+    if (!window.tour) window.tour = {
+        updateStops: function (newStops) {
+            stops = newStops;
+        },
+        // map: google map object
+        // directionsDisplay: google directionsDisplay object (comes in empty)
+        loadMap: function (map, directionsDisplay) {
+            var myOptions = {
+                zoom: 13,
+                center: new window.google.maps.LatLng(51.507937, -0.076188), // default to London
+                mapTypeId: window.google.maps.MapTypeId.ROADMAP
+            };
+            map.setOptions(myOptions);
+            directionsDisplay.setMap(map);
+        },
+        fitBounds: function (map) {
+            var bounds = new window.google.maps.LatLngBounds();
+
+            // extend bounds for each record
+            jQuery.each(stops, function (key, val) {
+                var myLatlng = new window.google.maps.LatLng(val.Geometry.Latitude, val.Geometry.Longitude);
+                bounds.extend(myLatlng);
+            });
+            map.fitBounds(bounds);
+        },
+        calcRoute: function (directionsService, directionsDisplay) {
+            var batches = [];
+            var itemsPerBatch = 10; // google API max = 10 - 1 start, 1 stop, and 8 waypoints
+            var itemsCounter = 0;
+            var wayptsExist = stops.length > 0;
+
+            while (wayptsExist) {
+                var subBatch = [];
+                var subitemsCounter = 0;
+
+                for (var j = itemsCounter; j < stops.length; j++) {
+                    subitemsCounter++;
+                    subBatch.push({
+                        location: new window.google.maps.LatLng(stops[j].Geometry.Latitude, stops[j].Geometry.Longitude),
+                        stopover: true
+                    });
+                    if (subitemsCounter == itemsPerBatch)
+                        break;
+                }
+
+                itemsCounter += subitemsCounter;
+                batches.push(subBatch);
+                wayptsExist = itemsCounter < stops.length;
+                // If it runs again there are still points. Minus 1 before continuing to 
+                // start up with end of previous tour leg
+                itemsCounter--;
+            }
+
+            // now we should have a 2 dimensional array with a list of a list of waypoints
+            var combinedResults;
+            var unsortedResults = [{}]; // to hold the counter and the results themselves as they come back, to later sort
+            var directionsResultsReturned = 0;
+
+            for (var k = 0; k < batches.length; k++) {
+                var lastIndex = batches[k].length - 1;
+                var start = batches[k][0].location;
+                var end = batches[k][lastIndex].location;
+
+                // trim first and last entry from array
+                var waypts = [];
+                waypts = batches[k];
+                waypts.splice(0, 1);
+                waypts.splice(waypts.length - 1, 1);
+
+                var request = {
+                    origin: start,
+                    destination: end,
+                    waypoints: waypts,
+                    travelMode: window.google.maps.TravelMode.WALKING
+                };
+                (function (kk) {
+                    directionsService.route(request, function (result, status) {
+                        if (status == window.google.maps.DirectionsStatus.OK) {
+
+                            var unsortedResult = { order: kk, result: result };
+                            unsortedResults.push(unsortedResult);
+                            
+                            directionsResultsReturned++;
+
+                            if (directionsResultsReturned == batches.length) // we've received all the results. put to map
+                            {
+                                // sort the returned values into their correct order
+                                unsortedResults.sort(function (a, b) { return parseFloat(a.order) - parseFloat(b.order); });
+                                var count = 0;
+                                for (var key in unsortedResults) {
+                                    if (unsortedResults[key].result != null) {
+                                        if (unsortedResults.hasOwnProperty(key)) {
+                                            if (count == 0) // first results. new up the combinedResults object
+                                                combinedResults = unsortedResults[key].result;
+                                            else {
+                                                // only building up legs, overview_path, and bounds in my consolidated object. This is not a complete 
+                                                // directionResults object, but enough to draw a path on the map, which is all I need
+                                                combinedResults.routes[0].legs = combinedResults.routes[0].legs.concat(unsortedResults[key].result.routes[0].legs);
+                                                combinedResults.routes[0].overview_path = combinedResults.routes[0].overview_path.concat(unsortedResults[key].result.routes[0].overview_path);
+
+                                                combinedResults.routes[0].bounds = combinedResults.routes[0].bounds.extend(unsortedResults[key].result.routes[0].bounds.getNorthEast());
+                                                combinedResults.routes[0].bounds = combinedResults.routes[0].bounds.extend(unsortedResults[key].result.routes[0].bounds.getSouthWest());
+                                            }
+                                            count++;
+                                        }
+                                    }
+                                }
+                                directionsDisplay.setDirections(combinedResults);
+                            }
+                        }
+                    });
+                })(k);
+            }
+        }
+    };
+}
